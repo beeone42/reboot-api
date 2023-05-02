595e18c55aa2f4fa4d346144509a427e99e8f7f3
Jordane Gengo <jordane@hive.fi>
2023-05-02 09:37:06 +0300

    feat: add decorator to check key

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ import subprocess, os, json, sys
 
 CONFIG_FILE = 'config.json'
 
-"""
-Open and load a file at the json format
-"""
+
+def check_key(func):
+    """
+    A decorator function that checks if the `key` is the same as the one in the config file
+    """
+    def wrapper(*args, **kwargs):
+        if (kwargs['key'] != config["key"]):
+            return "ko"
+        return func(*args, **kwargs)
+    return wrapper
+
 
 def open_and_load_config():
+    """
+    Open and load a file at the json format
+    """
     if os.path.exists(CONFIG_FILE):
         with open(CONFIG_FILE, 'r') as config_file:
             return json.loads(config_file.read())
@@ -20,14 +31,15 @@ def open_and_load_config():
 def hello():
     return "Reboot API"
 
+
 @route('/load')
 def load():
     return "%s, %s, %s" % os.getloadavg()
 
+
 @post('/<key>/reboot')
-def reboot(key):
-    if (key != config["key"]):
-        return "ko"
+@check_key
+def reboot():
     postdata = request.json
     status = postdata['heartbeat']['status']
     print("STATUS: %s" % status)
@@ -36,16 +48,17 @@ def reboot(key):
         subprocess.call(config["cmd"])
     return "ok"
 
+
 @post('/<key>/test')
-def test(key):
-    if (key != config["key"]):
-        return "ko"
+@check_key
+def test():
     postdata = request.json
     status = postdata['heartbeat']['status']
     print("STATUS: %s" % status)
     if (status == 0):
         print("Reboot needed")
     return "ok"
+
 
 if __name__ == "__main__":
     config = open_and_load_config()
